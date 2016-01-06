8f14bf6de2df9e5e5415e150231a3e0790773744
Marek Żebrowski <marek.zebrowski@gmail.com>
2016-01-06 19:33:36 +0100

    CCM

--- rogue-cc/src/main/scala/CCMacros.scala
+++ rogue-cc/src/main/scala/CCMacros.scala
@@ -21,6 +21,7 @@ object CCMacros {
 
   def impl(c:whitebox.Context)(annottees: c.Expr[Any]*): c.Expr[Any] = {
     import c.universe._
+    import c.universe.definitions._
 
     def extractCaseClassesParts(classDecl: ClassDef) = classDecl match {
       case q"case class $className(..$fields) extends ..$parents { ..$body }" =>
@@ -30,12 +31,55 @@ object CCMacros {
     def modifiedDeclaration(classDecl: ClassDef) = {
       val (className, fields, parents, body) = extractCaseClassesParts(classDecl)
 
-      val newFields = fields.asInstanceOf[List[ValDef]] map { p =>
+      def fieldObject(p: ValDef): ValDef = {
+
+        println(s"RHS ${p.rhs}")
+        println(s"TPT ${p.tpt}")
+        println(s"TPE ${p.tpe}")
+        p.tpt.foreach( t =>
+          println(s"  Inn ${t}")
+        )
+        println(s"tpt.tpe ${p.tpt.tpe}")
+        val tpt = p.tpt
+        println(s"isTerm ${tpt.isTerm}")
+        println(s"isDef ${tpt.isDef}")
+        println(s"isType ${tpt.isType}")
+        println(s"nonEMpty ${tpt.nonEmpty}")
+        //println(s"nonEMpty ${tpt.}")
+
+        p.tpt match {
+          case tq"Int" => println("Int field")
+          case tq"String" => println("String field")
+          case tq"Option[_]" => println("Optional field")
+          case _ => println("other field")
+        }
+  //      println("MEEE")
+        println(s"P: $p")
+        println(s"tpt ${p.tpt}")
+
+    //    println(s"Type ${p.tpe}")
+      //  println(s"$p")
+        val fName = p.name
+
+
+        //val fImpl = p.tpe match {
+          //case IntTpe => ModuleDef
+          //case _ =>
+
+//        }
+        //q"object $fName extends $fImpl"
         ValDef(Modifiers(),p.name,tq"String",q"${p.name.toString}")
+      }
+      //check if it is enough
+      val newFields = fields.asInstanceOf[List[ValDef]] map { p => fieldObject(p)
+
+        //ModuleDef(Modifiers(),TermName(p.name),Template())
+
       }
 
       val newName = TypeName(className.toString()+"Meta")
 
+      //maybe put meta inside ?
       val ex = c.Expr[Any](
         q"""
         $classDecl
